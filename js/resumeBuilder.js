4e38aa9576f2aa575ea067c83492fbbfcfae1e3a
Isaac Yi <isaac.yi@opower.com>
2016-09-09 18:12:54 -0400

    add javascript objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,76 @@
-$("#main").append("Isaac Yi");
+/*
+$("#main").append("Let's add stuff to the main section");
+
+var name = "Isaac Yi";
+var role = "Web Ninja";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
+
+*/
+
+var bio = {
+	"name" : "Isaac Yi",
+	"role" : "Web Ninja",
+	"welcomeMessage" : "I'm an aspiring web developer. Pick me!",
+	"biopic" : "images/fry.jpg",
+	"skills" : []
+	"contacts" : {
+		"mobile" : "6504270718",
+		"email" : "isaacyi89@gmail.com",
+		"github" : "",
+		"location" : "Washington DC"
+	}
+};
+/*
+$("#main").append(bio["name"]);
+$("#main").append(bio["role"]); 
+$("#main").append(bio.contact); 
+$("#main").append(bio.message, bio.picture);
+*/
+var education = {
+	"schools" : [{
+		"name" : "Stanford University",
+		"degree" : "B.S.",
+		"majors" : ["MS&E"],
+		"location" : "Palo Alto",
+		"degree dates" : 2012
+	}, {
+		"name" : "Stanford University",
+		"degree" : "M.S.", 
+		"major" : ["ERE"],
+		"location" : "Palo Alto",
+		"degree dates" : 2014
+	}]
+}
+
+var work = {
+	"jobs" : [{
+		"employer" : "",
+		"title" : "",
+		"location" : "",
+		"dates" : ""
+	}, {
+		"employer" : "",
+		"title" : "",
+		"location" : "",
+		"dates" : ""
+	}, {
+		"employer" : "",
+		"title" : "",
+		"location" : "",
+		"dates" : ""
+	}]
+}
+
+var project = {
+	"projects" : [{
+		"title" : "",
+		"dates" : "",
+		"description" : "",
+		"images" : []
+	}]
+}
+
