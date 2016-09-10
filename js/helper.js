8618f17a20d7c05e2b48c87f02cbf933009308f4
Isaac Yi <isaac.yi@opower.com>
2016-09-10 14:05:34 -0400

    display objects, encapsulation, google map

--- js/helper.js
+++ js/helper.js
@@ -1,11 +1,7 @@
 /*
 This file contains all of the code running in the background that makes resumeBuilder.js possible. 
 We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make 
-any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
 */
-
 
 /*
 These are HTML strings. As part of the course, you'll be using JavaScript functions
@@ -86,6 +82,9 @@ function logClicks(x,y) {
 
 $(document).click(function(loc) {
   // your code goes here!
+  var x = loc.pageX;
+  var y = loc.pageY;
+  logClicks(x,y);
 });
 
 
@@ -238,7 +237,7 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -9,6 +9,19 @@ var formattedRole = HTMLheaderRole.replace("%data%", role);
 $("#header").append(formattedName);
 $("#header").append(formattedRole);
 
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+$("#header").append(formattedName);
+
+if (document.getElementsByClassName("education-entry").length === 0) {
+document.getElementById("education").style.display = "none";
+};
+
+if (document.getElementsByTagName('h1').length === 0) {
+    document.getElementById('header').style.display = 'none';
+};
+
+// List of CSS properties: https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Properties_Reference
+
 */
 
 var bio = {
@@ -16,7 +29,7 @@ var bio = {
 	"role" : "Web Ninja",
 	"welcomeMessage" : "I'm an aspiring web developer. Pick me!",
 	"biopic" : "images/fry.jpg",
-	"skills" : []
+	"skills" : ["curiosity", "determination", "tenacity"],
 	"contacts" : {
 		"mobile" : "6504270718",
 		"email" : "isaacyi89@gmail.com",
@@ -24,12 +37,7 @@ var bio = {
 		"location" : "Washington DC"
 	}
 };
-/*
-$("#main").append(bio["name"]);
-$("#main").append(bio["role"]); 
-$("#main").append(bio.contact); 
-$("#main").append(bio.message, bio.picture);
-*/
+
 var education = {
 	"schools" : [{
 		"name" : "Stanford University",
@@ -44,26 +52,26 @@ var education = {
 		"location" : "Palo Alto",
 		"degree dates" : 2014
 	}]
-}
+};
 
 var work = {
 	"jobs" : [{
-		"employer" : "",
-		"title" : "",
-		"location" : "",
-		"dates" : ""
+		"employer" : "Opower",
+		"title" : "Customer Success Engineer",
+		"location" : "Washington, DC",
+		"dates" : ["2014", "2015", "2016"]
 	}, {
-		"employer" : "",
-		"title" : "",
-		"location" : "",
-		"dates" : ""
+		"employer" : "Mercedes Benz",
+		"title" : "Business Analyst",
+		"location" : "Stuttgart, DEU",
+		"dates" : ["2012", "2013"]
 	}, {
-		"employer" : "",
-		"title" : "",
-		"location" : "",
-		"dates" : ""
+		"employer" : "Tesla",
+		"title" : "Service Operations Associate",
+		"location" : "Palo Alto, CA",
+		"dates" : ["2011"]
 	}]
-}
+};
 
 var project = {
 	"projects" : [{
@@ -72,5 +80,59 @@ var project = {
 		"description" : "",
 		"images" : []
 	}]
-}
+};
+
+// add bio.name to id = header using the HTMLheaderName format
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+$("#header").append(formattedName);
+
+// if there are values in bio.skills, add them to the HTML as id = skills
+if (bio.skills.length > 0){
+	// add skills header statement
+	$("#header").append(HTMLskillsStart);
+	var formattedSkill;
+	for (i=0; i < bio.skills.length; i++){
+		formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+		$("#skills").append(formattedSkill);
+	}
+};
+
+// add work info onto HTML with the correct format
+function displayWorkInfo () {
+	for (job in work.jobs){
+		// create new div for work experience with id = workExperience
+		$("#workExperience").append(HTMLworkStart);
+		formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		formattedJob = formattedEmployer.concat(formattedTitle);
+		$(".work-entry:last").append(formattedJob);
+	};
+};
+
+displayWorkInfo();
+
+function locationizer(work) {
+	var workLocation = [];
+	for (i in work.jobs){
+		workLocation.push(work.jobs[i].location);
+	};
+	return workLocation;
+};
+
+locationizer(work);
+
+// add project info on HTML with the correct format
+project.display = function (){
+	for (i in project.projects){
+		$("#projects").append(HTMLprojectStart);
+		var formattedTitle = HTMLprojectTitle.replace("%data", project.projects[i].title);
+		$(".project-entry:last").append(formattedTitle);
+		//blahblahblah add all the other project details
+	}
+};
+
+$("#mapDiv").append(googleMap);
+
+
+
 
